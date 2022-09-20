af90d51173b9a82f1883e727ccb116712671353f
Mohamed Hakem <mohamedhakem959628@gmail.com>
2022-09-20 10:35:51 +0200

    Update AboutCard

--- components/Home/AboutCard.js
+++ components/Home/AboutCard.js
@@ -3,11 +3,8 @@ import Image from 'next/image'
 const cardStyle = `relative mb-6 box-border flex w-auto flex-col rounded-2xl bg-[#0f0f0f] p-6 text-white drop-shadow-xl`
 const innerCardStyle = `relative flex-start box-border flex flex-row flex-nowrap items-center justify-start`
 const imageStyle = `relative inset-0 h-16 w-16 flex-shrink-0 rounded-full object-cover object-center`
-// const buttonStyle = `m-auto flex w-full items-center justify-center border border-[#383838] rounded-xl bg-transparent p-[10px] transition
-// hover:bg-[#383838] duration-300 active:scale-[.9]`
 
 const AboutCard = () => {
-  // const dragConstraints = { top: 0, left: 0, right: 0, bottom: 0 }
   return (
     <div className={cardStyle}>
       <div className={innerCardStyle}>
@@ -22,7 +19,7 @@ const AboutCard = () => {
         </div>
         <div className="ml-5 flex-1 text-white">
           <h1 className="font-sans text-2xl font-bold">Mohamed Hakem</h1>
-          <p className="text-md">Software Engineer</p>
+          <p className="text-md">All 5-star excellent-feedback Software Engineer, hire me on Upwork</p>
         </div>
       </div>
     </div>
