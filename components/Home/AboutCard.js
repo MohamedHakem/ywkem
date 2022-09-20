09ab14e46d83591b2a0d758f29a2cbb624254663
Mohamed Hakem <mohamedhakem959628@gmail.com>
2022-09-20 11:37:27 +0200

    Add Upwork link to AboutCard component

--- components/Home/AboutCard.js
+++ components/Home/AboutCard.js
@@ -3,11 +3,9 @@ import Image from 'next/image'
 const cardStyle = `relative mb-6 box-border flex w-auto flex-col rounded-2xl bg-[#0f0f0f] p-6 text-white drop-shadow-xl`
 const innerCardStyle = `relative flex-start box-border flex flex-row flex-nowrap items-center justify-start`
 const imageStyle = `relative inset-0 h-16 w-16 flex-shrink-0 rounded-full object-cover object-center`
-// const buttonStyle = `m-auto flex w-full items-center justify-center border border-[#383838] rounded-xl bg-transparent p-[10px] transition
-// hover:bg-[#383838] duration-300 active:scale-[.9]`
+const UpworkStyle = `font-bold text-green-500 underline underline-offset-8`
 
 const AboutCard = () => {
-  // const dragConstraints = { top: 0, left: 0, right: 0, bottom: 0 }
   return (
     <div className={cardStyle}>
       <div className={innerCardStyle}>
@@ -22,7 +20,16 @@ const AboutCard = () => {
         </div>
         <div className="ml-5 flex-1 text-white">
           <h1 className="font-sans text-2xl font-bold">Mohamed Hakem</h1>
-          <p className="text-md">Software Engineer</p>
+          <p className="text-md">
+            All <span className={`text-md ${UpworkStyle}`}>5-star</span>{' '}
+            excellent-feedback Software Engineer, hire me on{' '}
+            <a
+              className={`text-lg ${UpworkStyle}`}
+              href="https://www.upwork.com/freelancers/mkem"
+            >
+              Upwork
+            </a>
+          </p>
         </div>
       </div>
     </div>
