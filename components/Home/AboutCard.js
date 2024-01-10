e41c3a7671051b282c098e3d01dfe7f8e6bd76d2
Mohamed Hakem <mohamedhakem959628@gmail.com>
2024-01-10 20:31:10 +0200

    Style: Update text color - AboutCard.js

--- components/Home/AboutCard.js
+++ components/Home/AboutCard.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const cardStyle = `relative mb-6 box-border flex w-auto flex-col rounded-2xl bg-[#0f0f0f] p-6 text-white drop-shadow-xl`
 const innerCardStyle = `relative flex-start box-border flex flex-row flex-nowrap items-center justify-start`
 const imageStyle = `relative inset-0 h-16 w-16 flex-shrink-0 rounded-full object-cover object-center`
-const UpworkStyle = `font-bold text-green-500 underline underline-offset-8`
+const UpworkStyle = `font-bold text-yellow-300 underline underline-offset-8`
 
 const AboutCard = () => {
   return (
